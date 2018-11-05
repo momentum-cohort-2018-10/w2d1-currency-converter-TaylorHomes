a74c0608d81ce8c12d3038c36951935eeb1e30e4
Taylor Homes <taylor.homes01@gmail.com>
2018-11-05 14:47:10 -0500

    First Test

---
+++ currency.py
@@ -0,0 +1,37 @@
+# # convert =
+# # turn value is the same as value.
+# # rates =
+# # # value = (amount of money to convert)
+# # # "from" =
+# # # "to" =
+
+
+# # def convert_currency():
+# #     when "from" == "to"
+# #     return value
+
+
+# # tuple_one = [("USD", "EUR", 0.74)]
+
+# # # Make sure than when you call convert with from and to being equal, the re
+
+# ~~~~~~~~~~~~~~
+# tup1 = [('USD', 'EUR', 0.74)]
+# tup2 = (1, 2, 3, 4, 5, 6, 7)
+
+# print("tup1[0]: ", tup1[0][])
+# print("tup2[1:5]: ", tup2[1:5])
+
+# rates = USD, EUR
+
+
+# def convert(_from, to, value):
+#     if _from == to
+
+# ~~~~~~~~~~~~~~~~~~~~~
+# # convert (1,[("USD", "EUR", 0.74)], "USD", "EUR") = 0.74
+# # rates = [("USD", "EUR", 0.74), ("EUR", "JPY", 145.949)]
+
+
+def convert(rates, value, current, to):
+    return value
